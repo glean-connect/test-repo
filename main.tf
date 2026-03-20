461224b45ae6259f4db0bfa0a7779b942213f595
skier7519 <peter.zelinski@outlook.com>
2026-03-20 07:53:14 -0700

    Create main.tf
    Initial

---
+++ main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+##############################
+# Variables
+##############################
+
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "my-gke-cluster"
+}
+
+variable "network" {
+  type    = string
+  default = "default"
+}
+
+variable "subnetwork" {
+  type    = string
+  default = "default"
+}
+
+variable "node_count" {
+  type    = number
+  default = 3
+}
+
+variable "machine_type" {
+  type    = string
+  default = "e2-medium"
+}
+
+##############################
+# GKE Cluster
+##############################
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = var.network
+  subnetwork = var.subnetwork
+
+  ip_allocation_policy {}
+}
+
+##############################
+# Node Pool
+##############################
+
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${var.cluster_name}-node-pool"
+  cluster    = google_container_cluster.primary.name
+  location   = var.region
+
+  node_count = var.node_count
+
+  node_config {
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "
